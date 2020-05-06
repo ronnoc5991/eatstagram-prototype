015a7913f7812608697f8b834486fa204edd9338
Connor <cstreng95@gmail.com>
2020-05-06 09:59:02 +0200

    Adds toggle form feature... begins creation form development

--- src/index.js
+++ src/index.js
@@ -1,19 +1,41 @@
 import createDisplayCard from './display-card-factory.js'
 
-const displayContainer = document.getElementById("displayContainer");
+const cardDisplayContainer = document.getElementById("cardDisplayContainer");
+const formContainer = document.getElementById("formContainer");
+const newCardButton = document.getElementById("newCard");
+newCardButton.addEventListener('click', toggleFormDisplay);
 
-displayContainer.appendChild(createDisplayCard());
-displayContainer.appendChild(createDisplayCard());
-displayContainer.appendChild(createDisplayCard());
-displayContainer.appendChild(createDisplayCard());
-displayContainer.appendChild(createDisplayCard());
-displayContainer.appendChild(createDisplayCard());
-displayContainer.appendChild(createDisplayCard());
-displayContainer.appendChild(createDisplayCard());
-displayContainer.appendChild(createDisplayCard());
-displayContainer.appendChild(createDisplayCard());
-displayContainer.appendChild(createDisplayCard());
-displayContainer.appendChild(createDisplayCard());
-displayContainer.appendChild(createDisplayCard());
-displayContainer.appendChild(createDisplayCard());
-displayContainer.appendChild(createDisplayCard());
+function toggleFormDisplay() {
+    if (formContainer.className == "hidden") { //if hidden, make active
+        cardDisplayContainer.style.opacity = "0.5";
+        formContainer.className = "active";
+    } else if (formContainer.className == "active") { //if active, make hidden
+        cardDisplayContainer.style.opacity = "1";
+        formContainer.className = "hidden";
+    }
+}
+
+
+cardDisplayContainer.appendChild(createDisplayCard());
+cardDisplayContainer.appendChild(createDisplayCard());
+cardDisplayContainer.appendChild(createDisplayCard());
+cardDisplayContainer.appendChild(createDisplayCard());
+cardDisplayContainer.appendChild(createDisplayCard());
+cardDisplayContainer.appendChild(createDisplayCard());
+cardDisplayContainer.appendChild(createDisplayCard());
+cardDisplayContainer.appendChild(createDisplayCard());
+cardDisplayContainer.appendChild(createDisplayCard());
+cardDisplayContainer.appendChild(createDisplayCard());
+cardDisplayContainer.appendChild(createDisplayCard());
+cardDisplayContainer.appendChild(createDisplayCard());
+cardDisplayContainer.appendChild(createDisplayCard());
+
+
+
+
+// needs a logo
+//needs an add recipe form that can take photos
+//needs a random recipe feature that pulls up a random recipe
+//how does it pull it up?  Can you click on a photo to make it larger?
+//can we add several photos and scroll through them?
+//tag features like a blog?  tag by lunch/dinner/italian/soup/vegetables etc.
